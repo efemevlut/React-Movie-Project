8ced3d8edc5567c423eaba3f58e2ac38d57eae96
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-11-22 13:21:36 +0300

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Card} from "./components/Card.style";
+import {Card} from "./components/Card";
 
 function App() {
   return (
---
+++ src/components/Card/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {StyledCardWrapper, StyledText} from "./Card.style"
+
+export const Card = () => {
+    return (
+        <StyledCardWrapper>
+            <StyledText>CARD</StyledText>
+        </StyledCardWrapper>
+    )
+}
