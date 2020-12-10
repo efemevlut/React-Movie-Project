3f32fb29fb6a6dd252ff7fcf95ef64d2a73a5aa5
Mevlüt EFE <67636958+efemevlut@users.noreply.github.com>
2020-12-10 20:57:52 +0300

    Add keyUp function

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,14 +1,33 @@
-import {useRef, useContext} from 'react';
-import {MovieContext} from '../../pages/Movie';
-import {StyledSearchBox, StyledSearchInput, StyledSearchButton} from "./SearchBox.style";
+import { useRef, useContext } from "react";
+import { MovieContext } from "../../pages/Movie";
+import {
+  StyledSearchBox,
+  StyledSearchInput,
+  StyledSearchButton,
+} from "./SearchBox.style";
 
 export const SearchBox = () => {
-    const inputRef = useRef();
-    const {setSearchKeyword} = useContext(MovieContext)
-    return (
-        <StyledSearchBox>
-           <StyledSearchInput ref={inputRef}/>
-           <StyledSearchButton onClick={()=>setSearchKeyword(inputRef?.current?.value)}>Search</StyledSearchButton> 
-        </StyledSearchBox>
-    )
-}
+  const inputRef = useRef();
+  const clickRef = useRef();
+  const { setSearchKeyword } = useContext(MovieContext);
+
+    const keyUp = (e) => e.key ==="Enter" ? clickRef.current.click() : null
+        
+    const setSearchFunc = () => {
+        return(
+            setSearchKeyword(inputRef?.current?.value)   
+        )
+    };
+
+  return (
+    <StyledSearchBox>
+      <StyledSearchInput ref={inputRef} onKeyUp={keyUp}/>
+      <StyledSearchButton
+        onClick={setSearchFunc}
+        ref={clickRef}
+      >
+        Search
+      </StyledSearchButton>
+    </StyledSearchBox>
+  );
+};
